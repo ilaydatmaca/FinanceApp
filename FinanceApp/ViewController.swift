f15d72ec8a6916775088392d73fafb0b171c66b5
ilayda atmaca <atmacailyada@gmail.com>
2022-06-17 14:28:35 +0300

    Initial Commit

---
+++ FinanceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by İlayda Atmaca on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
