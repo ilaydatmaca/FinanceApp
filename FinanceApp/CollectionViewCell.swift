c7017d1c7d386ac86fadfc74a63eaf2e6e7739fc
sevvallatmaca <sevvallatmaca@gmail.com>
2022-06-21 11:18:53 +0300

    collection view cell, image, label

---
+++ FinanceApp/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  FinanceApp
+//
+//  Created by Şevval Atmaca on 21.06.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var coinImageView: UIImageView!
+    @IBOutlet weak var coinLabel: UILabel!
+    
+}
