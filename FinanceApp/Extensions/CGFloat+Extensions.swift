a5434ea4f99bdcc66f11da14c8b7b364e22c3819
Muhammed Karakul <mtkarakul@anadolubank.com.tr>
2022-06-22 16:28:19 +0300

    CGFloat extension added.

---
+++ FinanceApp/Extensions/CGFloat+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  CGFloat+Extensions.swift
+//  FinanceApp
+//
+//  Created by Muhammed Tayyib KARAKUL (Yazılım Geliştirme 2-Dönemsel Çalışan) on 22.06.2022.
+//
+
+import UIKit
+
+extension CGFloat {
+    static var halfSizeOfScreen: CGFloat {
+        UIScreen.main.bounds.width / 2
+    }
+}
