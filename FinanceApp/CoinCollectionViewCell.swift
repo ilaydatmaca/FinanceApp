840c1ed3c171a08dfda364c91cd0c0b6f550cfd4
sevvallatmaca <sevvallatmaca@gmail.com>
2022-06-22 17:17:57 +0300

    no message

--- FinanceApp/CoinCollectionViewCell.swift
+++ FinanceApp/CoinCollectionViewCell.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class CoinCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var coinText: UILabel!
-    @IBOutlet public var coinImage: UIImageView!
+    @IBOutlet weak private var coinText: UILabel!
+    @IBOutlet weak private var coinImage: UIImageView!
     
-    @IBOutlet weak var coinPrice: UILabel!
-    @IBOutlet weak var coinShortening: UILabel!
+    @IBOutlet weak private var coinPrice: UILabel!
+    @IBOutlet weak private var coinShortening: UILabel!
     
     func setup(with coin : Coin){
         coinText.text = coin.label
