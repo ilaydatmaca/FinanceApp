640a9db79f901f81c4a160f0a2620a4ee80e36fa
ilayda atmaca <atmacailyada@gmail.com>
2022-06-27 17:10:25 +0300

    UI for market page

--- FinanceApp/Controller/CoinCollectionViewCell.swift
+++ FinanceApp/Controller/CoinCollectionViewCell.swift
@@ -23,6 +23,9 @@ class CoinCollectionViewCell: UICollectionViewCell {
         coinShortening.text = coin.shortening
         coinPrice.text = coin.price
         imageButton.tag = coin.buttonID
+        coinName.font = UIFont(name:"Montserrat", size: 20.0)
+        coinShortening.font = UIFont(name:"Montserrat", size: 18.0)
+        coinPrice.font = UIFont(name:"Montserrat", size: 20.0)
     }
     
 }
