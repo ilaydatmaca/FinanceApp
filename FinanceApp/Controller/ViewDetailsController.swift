a2f412c3ba3b797224510130278269f51522b0bf
ilayda atmaca <atmacailyada@gmail.com>
2022-06-28 12:22:46 +0300

    file error

--- FinanceApp/Controller/ViewDetailsController.swift
+++ FinanceApp/Controller/ViewDetailsController.swift
@@ -44,14 +44,7 @@ class ViewDetailsController: UIViewController {
         volume.text = "$" + String(format: "%.2f", ViewDetailsController.currentCoin.volume / CGFloat.billion ) + "B"
         rank.text = "#" + String(ViewDetailsController.currentCoin.rank)
         btcPrice.text = String(format: "%.2f", ViewDetailsController.currentCoin.btcPrice) + " BTC"
-        
-        coinName.font = UIFont(name:"Montserrat-Medium", size: 18.0)
-        coinPrice.font = UIFont(name:"Montserrat-Medium", size: 19.0)
-        coinShortening.font = UIFont(name:"Montserrat-Medium", size: 18.0)
-        btcPrice.font = UIFont(name:"Montserrat-Medium", size: 17.0)
-        marketCap.font = UIFont(name:"Montserrat-Medium", size: 17.0)
-        volume.font = UIFont(name:"Montserrat-Medium", size: 17.0)
-        rank.font = UIFont(name:"Montserrat-Medium", size: 17.0)
+    
 
         btcPrice.textColor = UIColor(red: 0.3451, green: 0.4039, blue: 0.3333, alpha: 1.0)
         marketDatatitle.textColor = UIColor(red: 0.4471, green: 0.4471, blue: 0.4471, alpha: 1.0)
